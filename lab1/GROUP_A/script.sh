697d0fa5d97490b478471fc72b6f84f7893c755a
Stefan Tagarski <116569281+stefantagarski@users.noreply.github.com>
2024-08-22 03:31:44 +0200

    Add files via upload

---
+++ lab1/GROUP_A/script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+	echo "Insert name of file!"
+fi
+
+if [ $# -gt 1 ]
+then
+	echo "Too many arguments"
+fi
+
+output_file="$1"
+
+if [ -e "$output_file" ]
+then
+	rm "$output_file"
+fi
+
+
+for file in *.txt
+do
+	if [ -r "$file" ]&&[ ! -w "$file" ]&&[ -x "$file" ]
+	then
+		cat "$file" >> "$output_file"
+		echo >> "$output_file"
+	fi
+done
+
